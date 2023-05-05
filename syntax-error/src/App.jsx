62d42ad98474ccc66f2751a4e5db671e0cf02d70
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-06 03:11:18 +0530

    fixed minor styling bugs..

--- syntax-error/src/App.jsx
+++ syntax-error/src/App.jsx
@@ -1,15 +1,34 @@
 import About from './components/About'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Project from './components/Project'
 import Home from './components/Home'
 import Marquee from './components/Marquee'
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(()=>{
+    window.addEventListener("scroll", function() {
+      const myDiv = document.querySelector(".line");
+      const position = myDiv.getBoundingClientRect().top;
+      const screenHeight = window.innerHeight;
+    
+      if (position < screenHeight - 100) {
+        myDiv.classList.add("show");
+      }
+    });  
+
+  },[window.innerHeight])
   return (
     <div className='text-white'>
       <Home />
-      <Project />
-      <About/>
-      <Marquee/>
+      <div className='relative'>
+        {/* White Line */}
+        <div className='line w-0 border-[4px] border-solid border-white h-screen -z-10 sticky mx-auto top-0 sm:left-[50%]'>
+        </div>
+        <Project />
+        <About />
+      </div>
+      <Marquee />
     </div>
   )
 }
--- syntax-error/src/components/Marquee.jsx
+++ syntax-error/src/components/Marquee.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Marquee = () => {
   return (
-    <div className='absolute top-0 text-white'>
+    <div className='fixed top-0 text-[#969B9F]'>
      <marquee style={{height: "100%"}} direction="up" behavior="scroll" scrollamount="12" loop="infinite" >
-        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-6xl font-Kanit'>Web Development</p>
-        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-6xl font-Kanit'>App Development</p>
+        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-6xl font-Kanit'>Web Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-6xl font-Kanit'>App Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-6xl font-Kanit'>IT Consultant</p>&nbsp;
      </marquee>
     </div>
   )
