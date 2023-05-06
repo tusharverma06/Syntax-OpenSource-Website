01a37d6c61c26e158a16ddb4505c46740766c816
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-05-06 23:48:27 +0530

    created project page with about

--- syntax-error/src/components/About.jsx
+++ syntax-error/src/components/About.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-
-
 import aboutprofile from "../assets/images/aboutprofile.png";
 import aboutprofile2 from "../assets/images/aboutprofile2.png";
 import aboutprofile3 from "../assets/images/aboutprofile3.png";
 import abouthr from '../assets/images/abouthr.svg'
 import abouthr2 from '../assets/images/abouthr2.svg'
 import {motion} from 'framer-motion'
-
 // import Navbar2 from "./Navbar2";
 const About = () => {
 // used for rotation of planet in framer motion instead of uing state
     //   const [rotate, cycleRotate] = useCycle(0,360);
-
   return (
     <>
       <div className="" >
@@ -27,15 +23,15 @@ const About = () => {
             className="absolute top-[-375px] left-[-247px]"
           />
           <img src={aboutearth} alt="" className="absolute top-36" /> */}
-          <motion.div className="container  p-4 flex flex-col justify-around items-center mx-auto gap-10">
+          <motion.div className="container flex flex-col items-center justify-around gap-10 p-4 mx-auto">
           {/* <Navbar  /> */}
 <br />
 <br />
 
-            <section className="backdrop-blur-sm text-white flex justify-center gap-60 bg-opacity-80 pl-12 pt-10 rounded-lg ">
+            <section className="flex justify-center pt-10 pl-12 text-white rounded-lg backdrop-blur-sm gap-60 bg-opacity-80 ">
               <div className="w-[500px] h-[212px]">
                 {" "}
-                <h1 className="text-5xl font-Kanit mt-5">About</h1>
+                <h1 className="mt-5 text-5xl font-Kanit">About</h1>
               </div>
               <div className="bg-[#101010] pt-5 " >
                 <p className=" leading-7 pl-[40px] pr-[40px] text-lg">
@@ -52,7 +48,7 @@ const About = () => {
             </section>
             <section className="backdrop-blur-sm text-white flex justify-center gap-32 bg-opacity-80 pl-12 pt-[10px] pr-[48px] pb-[20px] rounded-lg  ">
               <div className="w-[500px] h-[212px]">
-                <h1 className="text-5xl font-Kanit mt-5 whitespace-nowrap">
+                <h1 className="mt-5 text-5xl font-Kanit whitespace-nowrap">
                   What we do
                 </h1>
               </div>
@@ -73,7 +69,7 @@ const About = () => {
           </motion.div>
         </motion.div>
         <div className="flex flex-col gap-4 mb bg-[#0B0B0B] bg-fixed ">
-          <div className=" text-white flex ml-5 mt-8 gap-10  pl-32 pt-10  rounded-lg">
+          <div className="flex gap-10 pt-10 pl-32 mt-8 ml-5 text-white rounded-lg ">
             <div>
               <img src={aboutprofile} className="w-[100vw]" alt="" />
             </div>
@@ -81,8 +77,8 @@ const About = () => {
               <h1 className="font-Kanit text-[55px] font-semibold">
                 ROHIT GHOSH
               </h1>
-              <p className="leading-6 text-xl">FOUNDER & CEO</p>
-              <p className="w-4/5 leading-8 text-xl mt-10 font-Montserrat">
+              <p className="text-xl leading-6">FOUNDER & CEO</p>
+              <p className="w-4/5 mt-10 text-xl leading-8 font-Montserrat">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
                 blanditiis deserunt exercitationem totam laudantium cum
                 consequuntur veritatis saepe consectetur nam error mollitia
@@ -94,7 +90,7 @@ const About = () => {
             </div>
           </div>
 <img src={abouthr} className="w-full mt-8" alt="" />
-          <div className=" text-white flex ml-5 mt-8  gap-10  pl-32 pt-10  rounded-lg">
+          <div className="flex gap-10 pt-10 pl-32 mt-8 ml-5 text-white rounded-lg ">
             <div>
               <img src={aboutprofile2} className="w-[100vw]" alt="" />
             </div>
@@ -102,8 +98,8 @@ const About = () => {
               <h1 className="font-Kanit text-[55px] font-semibold">
               VASUDHA SINGH
               </h1>
-              <p className="leading-6 text-xl">FOUNDER & CEO</p>
-              <p className="w-4/5 leading-8 text-xl mt-10 font-Montserrat">
+              <p className="text-xl leading-6">FOUNDER & CEO</p>
+              <p className="w-4/5 mt-10 text-xl leading-8 font-Montserrat">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
                 blanditiis deserunt exercitationem totam laudantium cum
                 consequuntur veritatis saepe consectetur nam error mollitia
@@ -116,7 +112,7 @@ const About = () => {
           </div>
           <img src={abouthr2} className="w-full mt-8" alt="" />
 
-          <div className=" text-white flex ml-5 mb-8 mt-8 gap-10  pl-32 pt-10  rounded-lg">
+          <div className="flex gap-10 pt-10 pl-32 mt-8 mb-8 ml-5 text-white rounded-lg ">
             <div>
               <img src={aboutprofile3} className="w-[100vw]" alt="" />
             </div>
@@ -124,8 +120,8 @@ const About = () => {
               <h1 className="font-Kanit text-[55px] font-semibold">
              ARYA SAWANT
               </h1>
-              <p className="leading-6 text-xl">FOUNDER & CEO</p>
-              <p className="w-4/5 leading-8 text-xl mt-10 font-Montserrat">
+              <p className="text-xl leading-6">FOUNDER & CEO</p>
+              <p className="w-4/5 mt-10 text-xl leading-8 font-Montserrat">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
                 blanditiis deserunt exercitationem totam laudantium cum
                 consequuntur veritatis saepe consectetur nam error mollitia
---
+++ syntax-error/src/components/Projectandabout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Projectandabout = () => {
+  return (
+    <div  className='h-full'>
+        <div className='line w-0 border-[4px] border-solid border-white h-screen z-10 sticky mx-auto top-0 sm:left-[50%]'>
+        <div className='relative w-48 lg:w-64 border-t-8 border-l-8 rounded-lg h-28 top-1/4 left-[-12rem] lg:left-[-16rem]'>
+        </div>
+        <div className='relative w-48 lg:w-64  rounded-lg h-28 top-1/4 left-[-18rem] lg:left-[-24rem] text-center font-Kanit lg:bg-white  lg:bg-opacity-25   lg:backdrop-blur-sm'>
+            Project
+        </div>
+        <div className='relative w-48 text-center  rounded-lg lg:w-64 h-28 top-1/2 left-[8rem] font-Kanit lg:bg-white  lg:bg-opacity-25   lg:backdrop-blur-sm'>
+            About
+        </div>
+        <div className='relative w-48 border-b-8 border-r-8 rounded-lg lg:w-64 h-28 top-1/2 left-1/2'>
+        </div>
+     
+
+
+       
+       
+        </div>
+    </div>
+  )
+}
+
+export default Projectandabout
