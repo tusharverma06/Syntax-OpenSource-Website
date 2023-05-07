23ad29243f8b8082269f056837d36b79e8b0d891
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-05-07 15:02:12 +0530

    fixed some responsiveness issue in home page

--- syntax-error/src/components/Project/Projectandabout.jsx
+++ syntax-error/src/components/Project/Projectandabout.jsx
@@ -4,16 +4,16 @@ const Projectandabout = () => {
   return (
     <div  className='h-full'>
         <div className='line w-0 border-[4px] border-solid border-white h-screen z-10 sticky mx-auto top-0 sm:left-[50%]'>
-        <div className='relative w-48 lg:w-64 border-t-8 border-l-8 rounded-t-lg h-28 top-1/4 left-[-12rem] lg:left-[-16rem]'>
-        </div>
-        <div className='relative w-48 lg:w-64 flex items-center justify-center text-2xl rounded-xl h-28 top-1/4 left-[-18rem] lg:left-[-24rem] text-center font-Kanit lg:bg-white  lg:bg-opacity-25   lg:backdrop-blur-sm'>
-            Project
-        </div>
-        <div className='relative w-48 text-center flex items-center justify-center text-2xl rounded-xl lg:w-64 h-28 top-1/3 left-[8rem] font-Kanit lg:bg-white  lg:bg-opacity-25   lg:backdrop-blur-sm'>
-            About
-        </div>
-        <div className='relative w-48 border-b-8 border-r-8 rounded-b-lg lg:w-64 h-28 top-1/3 left-1/2'>
-        </div>
+          <div className='relative w-24 lg:w-64 border-t-8 border-l-8 rounded-t-lg h-28 top-1/4 left-[-6rem] lg:left-[-16rem]'>
+          </div>
+          <div className='relative w-24 lg:w-64 flex items-center justify-center text-2xl rounded-xl h-16 lg:h-28 top-1/4 left-[-9rem] lg:left-[-24rem] text-center font-Kanit lg:bg-white  lg:bg-opacity-25 border-2   lg:backdrop-blur-sm'>
+              Project
+          </div>
+          <div className='relative w-24 text-center flex items-center justify-center text-2xl rounded-xl h-16 lg:w-64 lg:h-28 top-1/3 left-[3rem] lg:left-[8rem] font-Kanit lg:bg-white  lg:bg-opacity-25   lg:backdrop-blur-sm border-2'>
+              About
+          </div>
+          <div className='relative w-24 border-b-8 border-r-8 rounded-b-lg lg:w-64 h-28 top-1/3 left-1/2'>
+          </div>
         </div>
     </div>
   )
