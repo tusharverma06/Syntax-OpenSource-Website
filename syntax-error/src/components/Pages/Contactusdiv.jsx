8a90a6bb19e1a37c98abef18a11ef4dbe874621b
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-05-09 16:47:57 +0530

    created animation for contact us page

--- syntax-error/src/components/Pages/Contactusdiv.jsx
+++ syntax-error/src/components/Pages/Contactusdiv.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import { Element } from "react-scroll";
+import { useRef } from 'react'
+import { useScroll, useTransform ,motion} from 'framer-motion'
 
 const Contactusdiv = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "end start"],
+  });
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 2]);
+  const height = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
   return (
     <Element name="Contact">
-      <div className='relative flex items-center justify-center h-screen '>
+      <div className='relative flex items-center justify-center h-screen ' ref={targetRef}>
               {/* White line */}
-              <div className='w-0 border-[0.375rem] h-[45%] border-solid border-white  absolute top-0 z-[-1]  mx-auto  sm:left-[50%]'>
-              </div>
+              <motion.div className='w-0 border-[0.375rem] h-[45%] border-solid border-white  absolute top-0 z-[-1]  mx-auto  sm:left-[50%]' style={{height}} >
+              </motion.div>
               {/* Contact us div */}
-              <div className='px-10 text-lg tracking-wider bg-white bg-opacity-25 rounded-md backdrop-blur-sm py-7 lg:text-4xl font-Kanit'>
+              <motion.div className='px-10 text-lg tracking-wider bg-white bg-opacity-25 rounded-md backdrop-blur-sm py-7 lg:text-4xl font-Kanit' style={
+                {opacity}
+              }>
                   CONTACT US 
-              </div>
+              </motion.div>
               {/* White line */}
-              <div className='w-0 border-[0.375rem] h-[45%] border-solid border-white  absolute bottom-0 z-[-1]  mx-auto  sm:left-[50%]'>
-              </div>
+              <motion.div className='w-0 border-[0.375rem] h-[45%] border-solid border-white  absolute bottom-0 z-[-1]  mx-auto  sm:left-[50%]' style={{height}}>
+              </motion.div>
       </div>
     </Element>
   )
