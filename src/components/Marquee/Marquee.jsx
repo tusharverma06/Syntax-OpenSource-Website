04d300d8e5bf1a892272076ebe25c7dfe50c8b3c
sahil-develops <zambanisahil99@gmail.com>
2023-06-09 12:33:09 +0530

    New Contact Page

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -7,6 +7,19 @@ const Marquee = () => {
         <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-4xl font-Kanit'>Web Development</p>&nbsp;
         <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>App Development</p>&nbsp;
         <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>IT Consultant</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-4xl font-Kanit'>Web Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>App Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>IT Consultant</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-4xl font-Kanit'>Web Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>App Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>IT Consultant</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-4xl font-Kanit'>Web Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>App Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>IT Consultant</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}} className='text-2xl uppercase lg:text-4xl font-Kanit'>Web Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>App Development</p>&nbsp;
+        <p   style={{writingMode: "vertical-lr"}}  className='mt-3 text-2xl uppercase lg:text-4xl font-Kanit'>IT Consultant</p>&nbsp;
+
      </marquee>
     </div>
   )
