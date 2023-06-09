e2221d05f4c596e01889c96b760b404693f90e39
sahil-develops <zambanisahil99@gmail.com>
2023-06-09 16:32:34 +0530

    Footer Fixed

--- src/components/Pages/Footer.jsx
+++ src/components/Pages/Footer.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import SElogo from '../../assets/images/SElogo.svg'
-
+import uparrow from '../../assets/images/uparrow.svg'
 const Footer = () => {
   return (
     <div>
-         <div className="relative z-10 flex flex-col px-4 md:flex-row justify-start  gap-5 bg-[#24272D] bg-opacity-90 pt-4 pb-4 pr-5">
-        <div className="mt-1 ml-5">
-          <h1 className="whitespace-nowrap font-extrabold font-Kanit uppercase flex justify-center text-[31px] ">
-          <img src={SElogo} width="150px"
-          />
-           </h1>
+         <div className="relative z-10 flex flex-col px-4 md:flex-row justify-center items-center gap-16 bg-[#161616]  pt-4 pb-4 pr-5">
+     <div className='sm:block hidden'>
+     <img src={uparrow} />
+     </div>
+        <div className='flex items-center '>
+     
+          <p className="text-lg font-light text-left font-Kanit ">
+           Based in Mumbai, India
+          </p>
 
         </div>
+        <div className="mt-1 ml-5">
+          <h1 className="whitespace-nowrap font-extrabold font-Kanit uppercase flex justify-center text-[28px] ">
+        SYNTAX  <img src={SElogo} width="50px" /> ERREUR
+            </h1>
+
+        </div>
+        
         <div>
           <p className="text-lg font-light text-left font-Kanit ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            vulputate libero et velit interdum, ac aliquet odio mattis. Class
-            aptent taciti sociosqu ad litora torquent per conubia nostra, per
-            inceptos himenaeos. Curabitur tempus urna at turpis condimentum .
+         Software Development Company © 2023
           </p>
 
         </div>
