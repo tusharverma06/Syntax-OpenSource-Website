8d3dce170b0487c7c159c1b51275e1cd3f738626
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-06-14 02:48:22 +0530

    made team card responsive

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,10 +15,10 @@ const Card = () => {
       title: {
         head: 'Aryan Singh',
         designation: 'Founder & CEO',
-        
-        img:Aryanser,
-        
-        name:'Twitter'
+
+        img: Aryanser,
+
+        name: 'Twitter'
       },
       description: 'The visionary founder of Syntax Erreur, leading the way in App and web development as a versatile full-stack developer'
     },
@@ -27,29 +27,29 @@ const Card = () => {
       title: {
         head: 'Chamuditha Jayood',
         designation: 'Founder',
-        img:vedant,
-        name:'Twitter'
+        img: vedant,
+        name: 'Twitter'
       },
       description: 'Founder of Syntax Erreur, mastering mobile and web development as a full-stack developer, with a passion for creating beautiful and functional user interfaces.'
-    }, 
+    },
     {
       image: aboutprofile2,
       title: {
         head: 'Vedant Sawant',
         designation: 'Cofounder',
-        img:noOne,
-        
-        name:'Twitter'
+        img: noOne,
+
+        name: 'Twitter'
       },
       description: 'A talented co-founder and skilled developer at Syntax Erreur, specializing in Django and backend development, crafting robust and dynamic web applications.'
-    }, 
+    },
     {
       image: aboutprofile,
       title: {
         head: 'Rohit Ghosh',
         designation: 'Cofounder',
-        img:noOne,
-        name:'Instagram'
+        img: noOne,
+        name: 'Instagram'
       },
       description: 'A dedicated co-founder at Syntax Erreur, contributing as a versatile full-stack developer, seamlessly bridging the gap between frontend and backend for exceptional results.      '
     },
@@ -58,52 +58,52 @@ const Card = () => {
       title: {
         head: 'Manas Mishra',
         designation: 'Cofounder',
-        img:noOne,
-        name:'Instagram'
+        img: noOne,
+        name: 'Instagram'
       },
       description: 'The creative co-founder of Syntax Erreur, bringing websites to life with his expertise in web development and a passion for innovative solutions. '
     },
     {
-        image: aboutprofile2,
-        title: {
-          head: 'Vasudha Singh',
-          designation: 'Core ',
-          img:noOne,
-          
-          name:'Twitter'
-        },
-        description: ' A vital core member at Syntax Erreur, diligently overseeing operations with exceptional managerial skills, ensuring smooth functioning and efficient execution'
-      },    {
-        image: aboutprofile3,
-        title: {
-          head: 'Monisha Keshwani',
-          designation: 'UI / UX Designer',
-          img:monisha,
-          name:'Twitter'
-        },
-        description: 'A valued core member at Syntax Erreur, channeling her passion for design into crafting captivating user friendly experiences as a UX/UI designer.'
-      },
-    {
-        image: aboutprofile2,
-        title: {
-          head: 'Arya Sawant',
-          designation: 'UX/UI Designer',
-          img:arya,
-          name:'Twitter'
-        },
-        description: ' A core member at Syntax Erreur, shaping exceptional user experiences as a talented UX/UI designer, combining creativity and technical expertise seamlessly.'
-      },
+      image: aboutprofile2,
+      title: {
+        head: 'Vasudha Singh',
+        designation: 'Core ',
+        img: noOne,
 
-      {
-        image: aboutprofile3,
-        title: {
-          head: 'Kaushik  Patil',
-          designation: 'Social Media Manager',
-          img:noOne,
-          name:'Twitter'
-        },
-        description: "The creative social media addict behind Syntax Erreur's instagram presence using marketing strategies to build a strong online presence and engage with our audience effectively.        "
-      }, 
+        name: 'Twitter'
+      },
+      description: ' A vital core member at Syntax Erreur, diligently overseeing operations with exceptional managerial skills, ensuring smooth functioning and efficient execution'
+    }, {
+      image: aboutprofile3,
+      title: {
+        head: 'Monisha Keshwani',
+        designation: 'UI / UX Designer',
+        img: monisha,
+        name: 'Twitter'
+      },
+      description: 'A valued core member at Syntax Erreur, channeling her passion for design into crafting captivating user friendly experiences as a UX/UI designer.'
+    },
+    {
+      image: aboutprofile2,
+      title: {
+        head: 'Arya Sawant',
+        designation: 'UX/UI Designer',
+        img: arya,
+        name: 'Twitter'
+      },
+      description: ' A core member at Syntax Erreur, shaping exceptional user experiences as a talented UX/UI designer, combining creativity and technical expertise seamlessly.'
+    },
+
+    {
+      image: aboutprofile3,
+      title: {
+        head: 'Kaushik  Patil',
+        designation: 'Social Media Manager',
+        img: noOne,
+        name: 'Twitter'
+      },
+      description: "The creative social media addict behind Syntax Erreur's instagram presence using marketing strategies to build a strong online presence and engage with our audience effectively.        "
+    },
 
   ];
 
@@ -112,28 +112,32 @@ const Card = () => {
       {array.map((item, index) => (
         <div
           key={index}
-          className='flex justify-center items-center bg-white w-[290px]  h-[340px]  sm:w-[350px] sm:h-[400px] text-black mx-auto rounded-2xl p-5 shadow-lg mr-6'
+          className='flex justify-center sm:justify-between items-start sm:items-center bg-white w-[290px]  h-[380px]  sm:w-[350px] sm:h-[400px] text-black mx-auto rounded-2xl p-5 shadow-lg mr-6'
         >
-          <div className='flex flex-col justify-start items-start'>
-            <div className='flex flex-row gap-5 justify-center items-center'>
-              <img src={item.title.img} alt=''  className='w-20 h-28 rounded-full sm:w-28 sm:h-28' />
+          <div className='flex flex-col items-center justify-around h-full sm:items-start'>
+            <div className='flex flex-col items-center justify-center gap-5 sm:flex-row'>
+              <img src={item.title.img} alt='' className='w-20 h-20 rounded-full sm:h-28 sm:w-28 ' />
               <div className='flex flex-col'>
                 <p className='font-semibold uppercase' >{item.title.head}</p>
-                <p>{item.title.designation}</p>
+                <p className='text-center'>{item.title.designation}</p>
                 {/* <p>Age: {item.title.age}</p> */}
               </div>
             </div>
 
             <div className='mt-5'>{item.description}</div>
-            <div className='flex justify-center mt-5'>
-              {/* <button className='flex items-center justify-between gap-2 px-5 py-2 text-white bg-black rounded-lg'>
-                Connect <img alt='downArrow' src={downArrow} className='mt-[6px] w-4 h-2' />
-              </button> */}
-              <br/>
+            <div className='flex justify-center w-full mt-3'>
+              <button className='flex items-center justify-between gap-2 px-5 py-2 text-white transition-all duration-1000 bg-black border border-black rounded-lg connect group hover:text-black hover:bg-white'>
+                Connect
+                <img className='mt-[6px] w-4 h-2 fill-black group-hover:hidden block transition-all duration-1000' xmlns="http://www.w3.org/2000/svg"
+                  width="20" height="8" src="https://img.icons8.com/ios-glyphs/16/ffffff/sort-down.png" alt="sort-down" />
+                <img className='mt-[6px] w-4 h-2 fill-black group-hover:block hidden transition-all duration-1000' xmlns="http://www.w3.org/2000/svg"
+                  width="20" height="8" src="https://img.icons8.com/ios-glyphs/16/000000/sort-down.png" alt="sort-down" />
+              </button>
+              <br />
             </div>
-              {/* <div className='flex justify-center  pt-2'>
-                <div className='bg-white  border-2 border-black rounded-xl'>
-<p className='flex justify-center items-center px-4  w-32 '><img src={item.title.img} width="50px"/> {item.title.name}</p>
+            {/* <div className='flex justify-center pt-2'>
+                <div className='bg-white border-2 border-black rounded-xl'>
+<p className='flex items-center justify-center w-32 px-4 '><img src={item.title.img} width="50px"/> {item.title.name}</p>
                 </div>
               </div> */}
           </div>
